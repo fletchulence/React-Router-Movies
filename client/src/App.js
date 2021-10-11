b57380a0c39f1b8184f222fc1c7a053f37b36a7c
David Fletcher <davidfletcher@Davids-Air-2.lan>
2021-10-11 13:24:41 -0700

    wrapped router over app -- initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ export default function App () {
       axios
         .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
         .then(response => {
+          console.log(response)
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
         })
