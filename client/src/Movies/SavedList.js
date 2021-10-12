9b2b30aad3cfbafee2d588b930c387fb17882614
David Fletcher <davidfletcher@Davids-Air-2-6.lan>
2021-10-12 09:02:18 -0700

    saved items are links

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,16 @@
 import React, {useEffect, useState} from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useRouteMatch, useHistory } from 'react-router-dom';
 
 export default function SavedList(props) {
+
 
   return (
     <div className="saved-list">
       <h3>Saved Movies:</h3>
-      {props.list.map((movie,index) => (
-        <span key={index} className="saved-movie">{movie.title}</span>
+      {props.list.map((movie, index) => (
+        <Link to={`/movies/${movie.id}`}>
+          <span key={index} className="saved-movie">{movie.title}</span>
+        </Link>
       ))}
       <Link to='/'>
         <div className="home-button">Home</div>
